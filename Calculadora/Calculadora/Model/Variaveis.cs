10bc72a6a133c8ed7783984807466374ffca0969
MiguelCarbonero <guelcarbonero@gmail.com>
2022-03-01 14:07:52 -0300

    Miguel - Inicial

---
+++ Calculadora/Calculadora/Model/Variaveis.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Model
+{
+    class Variaveis
+    {
+        public double valor1;
+        public double valor2;
+        public double resultado;
+        public string operador;
+        public string valorDigitado;
+    }
+}
---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Calculadora.Model;
+
+namespace Calculadora_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Title = "Calculadora";
+            Console.WriteLine("============================ Calculadora ============================ ");
+
+
+            // double valor1 = 0, valor2 = 0, resultado;
+            // string operador, valorDigitado;
+
+            Variaveis objvar = new Variaveis();
+
+
+            Console.WriteLine("Digite o 1 valor: ");
+            objvar.valorDigitado = Console.ReadLine();
+
+            objvar.valor1 = Math.Round(double.Parse(objvar.valorDigitado), 4);
+
+
+            Console.WriteLine("Digite a operação (' / | + | * | - '): ");
+            objvar.operador = Console.ReadLine();
+
+
+            Console.WriteLine("Digite o 2 valor: ");
+            objvar.valorDigitado = Console.ReadLine();
+
+            objvar.valor2 = Math.Round(double.Parse(objvar.valorDigitado), 4);
+
+
+            switch (objvar.operador)
+            {
+                case "+":
+                    objvar.resultado = Math.Round(objvar.valor1 + objvar.valor2, 4);
+                    Console.WriteLine($"{ objvar.valor1 } {'+'} { objvar.valor2 } = {objvar.resultado}");
+                    break;
+                case "-":
+                    objvar.resultado = Math.Round(objvar.valor1 - objvar.valor2, 4);
+                    Console.WriteLine($"{ objvar.valor1 } {'-'} { objvar.valor2 } = {objvar.resultado}");
+                    break;
+                case "*":
+                    objvar.resultado = Math.Round(objvar.valor1 * objvar.valor2, 4);
+                    Console.WriteLine($"{ objvar.valor1 } {'*'} { objvar.valor2 } = {objvar.resultado}");
+                    break;
+                case "/":
+                    objvar. resultado = Math.Round(objvar.valor1 / objvar.valor2, 4);
+                    Console.WriteLine($"{ objvar.valor1 } {'/'} { objvar.valor2 } = {objvar.resultado}");
+                    break;
+
+            }
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
